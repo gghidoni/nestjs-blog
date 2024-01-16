a566dc875f1563b76562c3e0ef2abdb26b407aa3
Gianni Ghidoni <gianni.ghidoni@rossotech.it>
2024-01-16 10:14:00 +0100

    fix db connection port

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-
+ 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27018/blog-db'),

a568e55bf302627e2189981c563278a078ee9601
Gianni <g.ghidoni3@gmail.com>
2024-02-10 21:51:38 +0100

    fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,19 +2,24 @@ import { Body, Controller, HttpCode, HttpStatus, Post, UsePipes, ValidationPipe 
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 import { UserModule } from "src/users/users.module";
+import { ApiCreatedResponse, ApiOkResponse, ApiResponse, ApiTags, ApiUnauthorizedResponse } from "@nestjs/swagger";
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('signup')
     @UsePipes(new ValidationPipe({whitelist: true}))
+    @ApiCreatedResponse({description: 'User registration'})
     signup(@Body() dto: AuthDto) {
         return this.authService.signup(dto);
     }
 
     @HttpCode(HttpStatus.OK)
     @Post('signin')
+    @ApiOkResponse({description: 'User login'})
+    @ApiUnauthorizedResponse({description: 'Invalid credentials'})
     signin(@Body() dto: AuthDto) {
         return this.authService.signin(dto);
     }
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,18 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class AuthDto {
+
+    @ApiProperty({type: String, description: 'User email'})
     @IsEmail()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty({type: String, description: 'User password'})
     @IsString()
     @IsNotEmpty()
     password: string;
 
+    @ApiProperty({type: String, description: 'User display name'})
     @IsOptional()
     @IsString()
     displayName?: string;
 
+    @ApiProperty({type: String, description: 'User avatar url'})
     @IsOptional()
     @IsString()
     avatarUrl?: string;
